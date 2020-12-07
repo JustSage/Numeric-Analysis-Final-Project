9beecf907a070ef1f2349cebdcd8afa52103c2a1
Sunspawn <nikolay.babkin@gmail.com>
2020-12-07 20:36:44 +0200

    working integration

---
+++ numproj.py
@@ -0,0 +1,45 @@
+import numpy
+
+
+def n_point_double_integral(f, a, b, c, d, n=2):
+    """double integral using two-point gauss method
+    
+    integral between -1 and 1 of f(x) = w0*f(x0) + w1*f(x1)
+    
+    :param f:       The function we want to integrate
+    :param a:       The lower bound over the x axis.
+    :param b:       The upper bound over the x axis.
+    :param c:       The lower bound over the y axis.
+    :param d:       The upper bound over the y axis.
+    :param n:       The number of points to use.
+    :return:        The value of the integral
+    """
+    # change the x and y values to fit an integral between -1 and 1
+    g = lambda x: (d+c)/2 + ((d-c)/2)*x
+    h = lambda y: (b+a)/2 + ((b-a)/2)*y
+    
+    # the legendre weights and values
+    xs, cs = numpy.polynomial.legendre.leggauss(n)
+    
+    # the inner integral over x
+    def inner_f(y):
+        sum = 0
+        for i in range(n):
+            sum += cs[i] * f(g(xs[i]), y) * ((d-c) / 2)
+        return sum
+    
+    # outer integral over y
+    sum = 0
+    for i in range(n):
+        sum += cs[i] * inner_f(h(xs[i])) * ((b-a) / 2)
+    return sum
+
+
+def f(x, y):
+    return x**2 + y**2
+
+
+# 484
+print(n_point_double_integral(f, 3, 9, 0, 2))
+# 8/3
+print(n_point_double_integral(f, -1, 1, -1, 1))
