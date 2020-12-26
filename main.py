7134c83f36abc85437ea33a4b8755391f47b0891
Sunspawn <nikolay.babkin@gmail.com>
2020-12-26 17:52:47 +0200

    Using new (bad) function to read input

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 from numproj import n_point_double_integral
-from userInput import read_polynomial, read_float
+from userInput import read_polynomial, read_float, read_func
 from math import ceil
 
 
-def main():
+if __name__ == "__main__":
     try:
-        poly = read_polynomial()
+        poly = read_func()
+        # poly = read_polynomial()
         a = read_float("Enter the lower bound of the integral for y.")
         b = read_float("Enter the upper bound of the integral for y.")
         c = read_float("Enter the lower bound of the integral for x.")
         d = read_float("Enter the upper bound of the integral for x.")
-        result = n_point_double_integral(poly.get_func(), a, b, c, d, ceil(poly.degree() / 2))
+        # result = n_point_double_integral(poly.get_func(), a, b, c, d, ceil(poly.degree() / 2))
+        result = n_point_double_integral(poly, a, b, c, d, 2)
         print("The approximation of the integral is " + str(result))
     except IOError as e:
         print(e)
-
-
-main()
