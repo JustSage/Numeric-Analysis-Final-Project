bb0ae0fb06f0de75b43dcb82f97dd014f63a61ce
Sunspawn <44163703+Sunspawn@users.noreply.github.com>
2021-02-05 12:53:59 +0200

    assignment decision
    decides which questions to pick based on id numbers of participants.

---
+++ assignment_decider.py
@@ -0,0 +1,35 @@
+"""
+
+code to decide which questions to pick based on the id numbers of the participants.
+
+"""
+
+ids = {
+    "Nikolay": 321123242,
+    "Sagi": 205591829,
+    "Alex": 317414506
+}
+
+options = (9, 8, 12, 6, 6)
+
+
+def decide(options):
+    base = sum(ids.values())
+    return base % options
+
+
+def make_decisions(all_options):
+    result = [decide(all_options[0]),
+              decide(all_options[1]) + options[0],
+              decide(all_options[2]) + options[0] + options[1],
+              decide(all_options[2]) + options[0] + options[1],
+              decide(all_options[3]) + options[0] + options[1] + options[2],
+              decide(all_options[4]) + options[0] + options[1] + options[2] + options[3]]
+    result[3] = (result[3] + 4) % options[2] + options[0] + options[1]
+    return result
+
+
+if __name__ == "__main__":
+    result = make_decisions(options)
+    for i in range(len(result)):
+        print("Question #{0}: {1}".format(i+1, result[i]))
