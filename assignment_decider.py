c726f50fa37f7fad43c9479a046974e3ec726586
Sunspawn <44163703+Sunspawn@users.noreply.github.com>
2021-02-05 19:19:26 +0200

    added random

--- assignment_decider.py
+++ assignment_decider.py
@@ -4,29 +4,33 @@ code to decide which questions to pick based on the id numbers of the participan
 
 """
 
+from random import randint
+
 ids = {
     "Nikolay": 321123242,
-    "Sagie": 205591829,
-    "Alex": 317414506,
-    "Avi": 203397666
-    }
+    "Sagi": 205591829,
+    "Alex": 317414506
+}
 
-options = (9, 8, 12, 6, 6)
+options = (9, 8, 12, 12, 6, 6)
 
 
 def decide(options):
     base = sum(ids.values())
-    return base % options
+    return (base + randint(0, options - 1)) % options
 
 
 def make_decisions(all_options):
     result = [decide(all_options[0]),
               decide(all_options[1]) + options[0],
               decide(all_options[2]) + options[0] + options[1],
-              decide(all_options[2]) + options[0] + options[1],
-              decide(all_options[3]) + options[0] + options[1] + options[2],
-              decide(all_options[4]) + options[0] + options[1] + options[2] + options[3]]
-    result[3] = (result[3] + 4) % options[2] + options[0] + options[1]
+              decide(all_options[3]) + options[0] + options[1],
+              decide(all_options[4]) + options[0] + options[1] + options[2],
+              decide(all_options[5]) + options[0] + options[1] + options[2] + options[3]]
+
+    while result[3] == result[2]:
+        result[3] = decide(all_options[3])
+
     return result
 
 
